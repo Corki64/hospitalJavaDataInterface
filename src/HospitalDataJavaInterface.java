74cb714b301372f55b75b955aed7c0d20a5e5e7b
Luis Cortez <lac0084@auburn.com>
2019-09-14 20:05:28 -0500

    Created methods to read and import data from 2 seperate delimited data files - per assignment -
    All tested - database has been loaded successfully.

--- src/HospitalDataJavaInterface.java
+++ src/HospitalDataJavaInterface.java
@@ -1,11 +1,16 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.DateTimeException;
-
 /**
  * Nearly a total rewrite of last program turn in. I hope I am doing this correctly. CPSC_5133
  *
@@ -16,8 +21,86 @@ import java.time.DateTimeException;
 public class HospitalDataJavaInterface {
     public static void main(String[] args) throws IOException {
         HospitalDataJavaInterface app = new HospitalDataJavaInterface();
+
+
         String location = "D://personTableData.txt";
         app.readPersonTableData(location);
+
+
+        String patientDoctorListLocation = "D:\\patientDoctorList.txt";
+        app.readPatientDoctorListData(patientDoctorListLocation);
+
+        String treatmentDataListLocation = "D:\\treatmentData.txt";
+        app.readTreatmentData(treatmentDataListLocation);
+    }
+
+    private void readTreatmentData(String treatmentDataListLocation) throws IOException {
+        String patient_last_name;
+        String doctor_last_name;
+        String treatment_type;
+        String treatment_timestamp;
+
+        BufferedReader buffReader = new BufferedReader(new FileReader(treatmentDataListLocation));
+        String line;
+
+        while ((line = buffReader.readLine()) != null) {
+            String[] values = line.split(",", -1);
+            patient_last_name = values[0];
+            doctor_last_name = values[1];
+            treatment_type = values[2];
+            treatment_timestamp = values[3];
+
+            treatmentListInsertMethod(patient_last_name,doctor_last_name,treatment_type,treatment_timestamp);
+        }
+        buffReader.close();
+    }
+
+    private void treatmentListInsertMethod(String patient_last_name, String doctor_last_name, String treatment_type, String treatment_timestamp) {
+        String sql = "insert into treatment_table(patient_last_name, doctor_last_name, treatment_type, treatment_timestamp) values (?,?,?,?)";
+        try (Connection conn = this.connect()) {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1,patient_last_name);
+            ps.setString(2,doctor_last_name);
+            ps.setString(3,treatment_type);
+            Timestamp sqlTimeStamp = new java.sql.Timestamp(new SimpleDateFormat("MM-dd-yyyy HH:mm").parse(treatment_timestamp).getTime());
+            ps.setTimestamp(4, sqlTimeStamp);
+            ps.executeUpdate();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void readPatientDoctorListData(String patientDoctorListLocation) throws IOException {
+        String patient_last_name;
+        String doctor_last_name;
+
+        BufferedReader buffReader = new BufferedReader(new FileReader(patientDoctorListLocation));
+        String line;
+
+        while ((line = buffReader.readLine()) != null) {
+            String[] values = line.split(",", -1);
+            patient_last_name = values[0];
+            doctor_last_name = values[1];
+
+            patientDoctorListInsertMethod(patient_last_name, doctor_last_name);
+        }
+        buffReader.close();
+    }
+
+    private void patientDoctorListInsertMethod(String patient_last_name, String doctor_last_name) {
+        String sql = "insert into patient_doctor_list(patient_name, doctor_name) values (?,?)";
+        try (Connection conn = this.connect()){
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, patient_last_name);
+            ps.setString(2, doctor_last_name);
+            ps.executeUpdate();
+            ps.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -28,20 +111,19 @@ public class HospitalDataJavaInterface {
     private void readPersonTableData(String location) throws IOException {
         // declare all variables for prepared statement
         String person_type;
-        String person_first_name = "";
-        String person_last_name = "";
-        String doctor_privilege = "";
+        String person_first_name;
+        String person_last_name;
+        String doctor_privilege;
         String patient_id;
-        String room_id = "";
-        String emergency_contact_name = "";
+        String room_id;
+        String emergency_contact_name;
         String emergency_contact_number;
-        String insurance_policy_number = "";
-        String insurance_policy_company = "";
-        String patient_doctor_last_name = "";
-        String initial_dx = "";
-        String admission_date = null;
-        String discharge_date = null;
-
+        String insurance_policy_number;
+        String insurance_policy_company;
+        String patient_doctor_last_name;
+        String initial_dx;
+        String admission_date;
+        String discharge_date;
         // implement buffered reader for text file
         BufferedReader buffReader = new BufferedReader(new FileReader(location));
         String line;
@@ -134,7 +216,7 @@ public class HospitalDataJavaInterface {
             ps.setString(12,initial_dx);
             try {
                 if (admission_date != null) {
-                    Date inDate = new java.sql.Date(((java.util.Date) new SimpleDateFormat("yyyy-MM-dd").parse(admission_date)).getTime());
+                    Date inDate = new java.sql.Date(new SimpleDateFormat("yyyy-MM-dd").parse(admission_date).getTime());
                     ps.setDate(13,inDate);
                 }
             } catch (DateTimeException | ParseException e) {
@@ -142,7 +224,7 @@ public class HospitalDataJavaInterface {
             }
             try {
                 if (discharge_date != null) {
-                    Date disDate = new java.sql.Date(((java.util.Date) new SimpleDateFormat("yyyy-MM-dd").parse(discharge_date)).getTime());
+                    Date disDate = new java.sql.Date(new SimpleDateFormat("yyyy-MM-dd").parse(discharge_date).getTime());
                     ps.setDate(14,disDate);
                 }
             } catch (DateTimeException | ParseException e) {
