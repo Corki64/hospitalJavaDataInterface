c74a8c340ad658b9a0b1e930e891b44871c6f371
Luis Cortez <lac0084@auburn.com>
2019-09-22 18:33:54 -0500

    All sub tables loaded correctly
    deleteAllRecordsFromDataBase() created - for testing purposes

--- src/HospitalDataJavaInterface.java
+++ src/HospitalDataJavaInterface.java
@@ -1,13 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.sql.Types;
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.DateTimeException;
@@ -22,6 +16,9 @@ public class HospitalDataJavaInterface {
     public static void main(String[] args) throws IOException {
         HospitalDataJavaInterface app = new HospitalDataJavaInterface();
 
+        //app.tableUpdate();
+
+        app.deleteAllRecordsFromDataBase();
 
         String location = "D://personTableData.txt";
         app.readPersonTableData(location);
@@ -32,6 +29,9 @@ public class HospitalDataJavaInterface {
 
         String treatmentDataListLocation = "D:\\treatmentData.txt";
         app.readTreatmentData(treatmentDataListLocation);
+        app.tableUpdate();
+
+
     }
 
     private void readTreatmentData(String treatmentDataListLocation) throws IOException {
@@ -69,7 +69,6 @@ public class HospitalDataJavaInterface {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     private void readPatientDoctorListData(String patientDoctorListLocation) throws IOException {
@@ -254,4 +253,109 @@ public class HospitalDataJavaInterface {
             }
             return conn;
         }
+
+    /**
+     * Test sample selector... testing method.
+     */
+    private void selectSample(){
+        String sql = "SELECT person_first_name, person_last_name FROM person;";
+        try (Connection conn = this.connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)){
+            while (rs.next()) {
+                System.out.println(rs.getString("person_first_name") + "\t" +
+                        rs.getString("person_last_name") + "\t");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteAllRecordsFromDataBase(){
+        String deleteAllRecords = "delete from administrator; delete from doctor; " +
+                "delete from in_patient; delete from nurse; delete from out_patient; delete from patient; " +
+                "delete from patient_doctor_list; delete from technician; " +
+                "delete from treatment_table; delete from volunteer; delete from employee; delete from person;";
+
+        try (Connection conn = this.connect();
+             Statement stmt = conn.createStatement()
+        ){
+            stmt.executeUpdate(deleteAllRecords);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Table update method. Wil distribute all data from person table to all other tables.
+     */
+    private void tableUpdate(){
+        String patientUpdate = "insert into patient(person_id, patient_id, patient_type) " +
+                "select person_id, patient_id, person_type from person " +
+                "where person_type = 'I' or person_type = 'O';";
+        String outPatientUpdate = "insert into out_patient(patient_id, person_type, person_first_name, " +
+                "person_last_name) select patient_id, person_type, person_first_name, person_last_name from person " +
+                "where person_type = 'O'";
+        String inPatientUpdate = "insert into in_patient(patient_id, person_type, room_id, emergency_contact_name, " +
+                "emergency_contact_number, insurance_policy_number, insurance_policy_company, " +
+                "patient_doctor_last_name, initial_dx, admission_date, discharge_date) select patient_id," +
+                " person_type, room_id, emergency_contact_name, emergency_contact_number, insurance_policy_number," +
+                " insurance_policy_company, patient_doctor_last_name, initial_dx, admission_date, discharge_date " +
+                "from person where person_type = 'I' or emergency_contact_name = not null " +
+                "and emergency_contact_number = not null and insurance_policy_number = not null " +
+                "and insurance_policy_company = not null and patient_doctor_last_name = not null;";
+        String employeeUpdate = "insert into employee(employee_first_name, employee_last_name, person_id," +
+                " employee_type, doctor_priv) " +
+                "select person_first_name, person_last_name, person_id, person_type, doctor_privilege from person " +
+                "where person_type = 'V' or person_type = 'D' or person_type = 'A' or person_type = 'N' " +
+                "or person_type = 'T';";
+        String volunteerUpdate = "insert into volunteer(person_first_name,person_last_name,person_id, employee_id) " +
+                "select employee_first_name, employee_last_name, person_id, employee_id from employee " +
+                "where employee_type = 'V';";
+        String doctorUpdate = "insert into doctor(person_first_name,person_last_name,person_id, " +
+                "employee_id, doctor_priv) " +
+                "select employee_first_name, employee_last_name, person_id, employee_id, doctor_priv from employee " +
+                "where employee_type = 'D' ";
+        String administratorUpdate = "insert into administrator(person_first_name,person_last_name,person_id," +
+                " employee_id) select employee_first_name, employee_last_name, person_id, employee_id from employee " +
+                "where employee_type = 'A';";
+        String nurseUpdate = "insert into nurse(person_first_name,person_last_name,person_id, employee_id) " +
+                "select employee_first_name, employee_last_name, person_id, employee_id from employee " +
+                "where employee_type = 'N';";
+        String technicianUpdate = "insert into technician(person_first_name,person_last_name,person_id, employee_id) " +
+                "select employee_first_name, employee_last_name, person_id, employee_id from employee " +
+                "where employee_type = 'T';";
+
+        try (Connection conn = this.connect();
+             Statement stmt = conn.createStatement()
+        ){
+            stmt.executeUpdate(patientUpdate);
+            stmt.executeUpdate(outPatientUpdate);
+            stmt.executeUpdate(inPatientUpdate);
+            stmt.executeUpdate(employeeUpdate);
+            stmt.executeUpdate(volunteerUpdate);
+            stmt.executeUpdate(doctorUpdate);
+            stmt.executeUpdate(administratorUpdate);
+            stmt.executeUpdate(nurseUpdate);
+            stmt.executeUpdate(technicianUpdate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
